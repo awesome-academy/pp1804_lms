edca0be5557210c77cb9f83319dcbb9c02fd4d64
Doanh <doanhdev@gmail.com>
2019-01-15 04:49:35 +0700

    create_static_page_backend

---
+++ resources/views/backend/index.blade.php
@@ -0,0 +1,25 @@
+@extends('backend.master')
+
+@section('title', 'Dashboard')
+
+@section('header')
+    
+@endsection
+
+@section('body')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Blank</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /.container-fluid -->
+
+@endsection
+
+@section('footer')
+    
+@endsection
